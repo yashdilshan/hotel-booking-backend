206cd5d3d58d690e2b80743905fd74e5ffa9907a
yashdilshan <yashodadilshan@gmail.com>
2024-12-01 18:54:59 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import bodyParser from "body-parser";
 
 const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(5000, (req, res) => {
     console.log("The program runs on port 5000");
