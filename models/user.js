46e3bb718d4e4b2ef01703709879c38174d042ed
yashdilshan <yashodadilshan@gmail.com>
2024-12-02 23:24:16 +0530

    Modify user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
     email: {
         type: String,
         required: true,
