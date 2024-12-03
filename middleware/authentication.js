00a8c9c1b0dd2c77f18852116df991a45b38d5c3
yashdilshan <yashodadilshan@gmail.com>
2024-12-03 18:36:40 +0530

    Created JWT authentication middleware to verify token and attach user info to request

---
+++ middleware/authentication.js
@@ -0,0 +1,13 @@
+export default function authenticate(req, res, next) {
+
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (token) {
+        jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (decoded) {
+                req.user = decoded;
+                next();
+            }
+            else next();
+        })
+    } else next();
+}
