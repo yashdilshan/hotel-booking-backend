b23f7e475361ede3f50053ca52fb257ffe3145cb
yashdilshan <yashodadilshan@gmail.com>
2025-01-02 16:59:58 +0530

    Database connection done

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 
 dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
+
+// database connection
+mongoose.connect(process.env.CONNECTION_STRING).then(() => console.log("Database connection success")).catch(() => console.log("Database connection fail"));
 
 app.listen(5000, (req, res) => {
     console.log("The program runs on port 5000");
