6c97ad813f9dc5ae0f264073829521ad3c606f64
yashdilshan <yashodadilshan@gmail.com>
2025-01-05 22:47:08 +0530

    Find by name API done

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -32,7 +32,7 @@ export function persist(req, res) {
 }
 
 export function retrieve(req, res) {
-    Event.find({ disabled: req.query.disabled })
+    Event.find({ disabled: req.query.disabled }).sort({ id: -1 })
         .then((events) => {
             if (events.length === 0) {
                 return res.status(404).json({ message: "Event not found" });
@@ -42,4 +42,20 @@ export function retrieve(req, res) {
         .catch((err) => {
             res.status(500).json({ message: "Server error occurred", error: err.message });
         })
+}
+
+export function findByName(req, res) {
+    Event.findOne({ name: { $regex: new RegExp(`^${req.params.name}$`, 'i') } }) // name case sensitivity remove.
+        .then((event) => {
+            if (!event) {
+                return res.status(404).json({ message: "Event Not found" });
+            }
+            res.status(200).json({
+                message: "Event found",
+                event: event
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({ message: "Server error occurred", error: err.message });
+        });
 }
--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { persist, retrieve } from "../controllers/eventController.js";
+import { findByName, persist, retrieve } from "../controllers/eventController.js";
 
 const eventRouter = express.Router();
 
 eventRouter.post("/", persist);
 
 eventRouter.get("/", retrieve);
+
+eventRouter.get("/name/:name", findByName);
 
 export default eventRouter;
 
