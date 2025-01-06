ea46000c3a66242f301c2bc4ee1a8ffc4fb70451
yashdilshan <yashodadilshan@gmail.com>
2025-01-06 22:48:27 +0530

    Remove review API done

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -96,3 +96,17 @@ export function update(req, res) {
             res.status(500).json({ message: "Server error occurred", error: err.message });
         })
 }
+
+export async function remove(req, res) {
+    if (!isHaveUser(req)) {
+        return res.status(401).json({ message: "User access required" });
+    }
+
+    Review.deleteOne({ id: req.params.id })
+        .then(() => {
+            res.status(200).json({ message: "Review Delete Successful" });
+        })
+        .catch((err) => {
+            res.status(500).json({ message: "Server error occurred", error: err.message });
+        })
+}
--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { findByEmail, findByName, persist, retrieve, update } from "../controllers/reviewController.js";
+import { findByEmail, findByName, persist, remove, retrieve, update } from "../controllers/reviewController.js";
 
 const reviewRouter = express.Router();
 
@@ -12,5 +12,7 @@ reviewRouter.get("/name/:name", findByName);
 reviewRouter.get("/email/:email", findByEmail);
 
 reviewRouter.put("/", update);
+
+reviewRouter.delete("/:id", remove);
 
 export default reviewRouter;
